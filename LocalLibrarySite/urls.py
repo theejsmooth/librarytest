4896e0e7e667586751e43b0b9b6d0d8873f4c71c
Josh <joshwilsonbz@gmail.com>
2018-12-08 02:38:00 -0800

    url patterns list and  account page

--- LocalLibrarySite/urls.py
+++ LocalLibrarySite/urls.py
@@ -22,11 +22,20 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('catalog/', include('catalog.urls')),
-]
-urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+
 ]
+#urlpatterns += [
+ #   path('catalog/', include('catalog.urls'))
+  #  ,
+#]
+#urlpatterns += [
+ #   path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+#]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#urlpatterns += [
+   # path('accounts/', include('django.contrib.auth.urls')),
+#]
